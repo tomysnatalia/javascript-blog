309120ee35ed55305c0b1a5081f7d6b463b9c42b
Natalia Tomys <hanf.natalia@gmail.com>
2020-10-28 18:10:17 +0100

    add class'active'

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 'use strict';
 
 function titleClickHandler(){
+  const clickedElement=this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
   }
 
-  /* remove class 'active' from all articles */
-
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles) {
   	activeArticle.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  // console.log('clickedElement (with plus): ' + clickedElement);
+  clickedElement.classList.add('active');
 
   /* get 'href' attribute from the clicked link */
 
